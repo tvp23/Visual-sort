3a050ddf76318e3f6083b8a7f626139409bee8f8
Twan van Paridon <70744506+tvp23@users.noreply.github.com>
2021-01-29 12:49:35 +0100

    update

--- js/visualbubble.js
+++ js/visualbubble.js
@@ -1,26 +1,29 @@
-// random numbers
+document.body.addEventListener('click', sortgraph);
+
+// vars
 numbers = Array();
-items = 1200;
+items = 2400;
+var itemssorted = 0;
 
 
 for(i=0;i<items;i++){
-    numbers.push(Math.floor(Math.random() * 300) + 10);
+    numbers.push(Math.floor(Math.random() * 500) +1);
     var graph = document.createElement('div');
     graph.setAttribute('id', i);
     graph.setAttribute('class', 'graphstyle');
     document.getElementById('graphcon').appendChild(graph);
     document.getElementById(i).style.height = numbers[i]+'px';
-    document.getElementById(i).style.width = '1px';
+    document.getElementById(i).style.width = '0.5px';
 }
 
 function sortgraph(){
-    for(x=0;x<1200;x++){
-        myVar = setTimeout(sort, 500);
+    for(x=0;x<2400;x++){
+        aVar = setTimeout(sort, 100);
     }
 }
 
 function sort(){
-    for(i=0;i<1200;i++){
+    for(i=0;i<2400;i++){
         if(numbers[i]>numbers[i+1]){
             var mem = numbers[i+1];
             numbers[i+1] = numbers[i];
